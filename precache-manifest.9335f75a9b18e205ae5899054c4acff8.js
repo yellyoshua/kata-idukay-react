8f1976c71116d50c8fb81a3e6d78a0d3ba05900b
yellyoshua <37986976+yellyoshua@users.noreply.github.com>
2021-11-18 03:26:26 +0000

    Deploying to gh-pages from @ yellyoshua/kata-idukay-react@5374290bc65dfed67e0a14c4965807fc7ad46f55 🚀

---
+++ precache-manifest.9335f75a9b18e205ae5899054c4acff8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0194c47865bd9593be06453ef549cf7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d44ae2cef4951bbd7f6",
+    "url": "/static/css/main.17ed5d1b.chunk.css"
+  },
+  {
+    "revision": "8e2e77f775b6f19edbc9",
+    "url": "/static/js/2.dd51ff82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dd51ff82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d44ae2cef4951bbd7f6",
+    "url": "/static/js/main.daecf5cd.chunk.js"
+  },
+  {
+    "revision": "39d90d752888c050b3e8",
+    "url": "/static/js/runtime-main.eca5dac1.js"
+  },
+  {
+    "revision": "16069de0f7f2a1dd14033cec82af0cdd",
+    "url": "/static/media/defame-potion.16069de0.png"
+  },
+  {
+    "revision": "aaebdadf56534f1ef7e2f51b9e1ad386",
+    "url": "/static/media/fake-friends-potion.aaebdadf.png"
+  },
+  {
+    "revision": "edd8ac1a381cf1ba228086dcb2f59f20",
+    "url": "/static/media/illusions-potion.edd8ac1a.png"
+  },
+  {
+    "revision": "1701359c946e05f3ad3e154f6819f395",
+    "url": "/static/media/love-potion.1701359c.png"
+  },
+  {
+    "revision": "f3503255c23bc7c8ccfd78152033cf92",
+    "url": "/static/media/smurf.f3503255.png"
+  },
+  {
+    "revision": "e752f791ad901f4309f4cbc6298a1dd2",
+    "url": "/static/media/thor.e752f791.png"
+  },
+  {
+    "revision": "ad58925ef644d46d3ea8a4e84b6afec9",
+    "url": "/static/media/wizard-original.ad58925e.png"
+  },
+  {
+    "revision": "bda6ae0015d83996e6a5ef1f36232938",
+    "url": "/static/media/wonder-woman.bda6ae00.png"
+  }
+]);
